403191f16749a80c3962a68424fd17ccfa33f55d
ronny karani <karanironny25@gmail.com>
2021-03-17 14:02:37 +0300

    added static and template files

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')), 
+    # ensures that whenever a URL that starts with catalog/  is received, the URLConf module catalog.urls will process the remaining substring.
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
