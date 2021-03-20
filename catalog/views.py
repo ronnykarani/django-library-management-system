e7024eb969cd428a76a9ad9b806dacd771ef37b7
ronny karani <karanironny25@gmail.com>
2021-03-20 09:35:21 +0300

    added sessions and saving session data

--- catalog/views.py
+++ catalog/views.py
@@ -12,13 +12,17 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+    num_authors = Author.objects.count() # # The 'all()' is implied by default.
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genre': num_genre
+        'num_genre': num_genre,
+        'num_visits': num_visits,
     }
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
