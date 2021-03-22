06854a6a57a1222d25a031bdf3f6cf95b8db1c6f
ronny karani <karanironny25@gmail.com>
2021-03-22 09:27:36 +0300

    added borrowed book instance and displayed it

--- catalog/admin.py
+++ catalog/admin.py
@@ -54,14 +54,14 @@ class BookInstanceAdmin(admin.ModelAdmin):
     - filters that will be displayed in sidebar (list_filter)
     - grouping of fields into sections (fieldsets)
     """
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
 
--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -69,12 +71,20 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
         """String for representing the Model object."""
         return '{0} ({1})'.format(self.id, self.book.title)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 # AUTHOR MODEL
 class Author(models.Model):
