811a41fb10b08193f83ea5bf72cee37982508ec4
ronny karani <karanironny25@gmail.com>
2021-03-23 06:15:36 +0300

    more on forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
